50c30b4919c97bcc0fa64bf667cefc6a16e5075e
BluVshal <blu.vishal@gmail.com>
2021-08-09 19:56:34 +0400

    First dev commit

---
+++ src/SampleLinkedIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SampleLinkedIn.css';
+
+function SampleLinkedIn() {
+  return (
+    <div className="app">
+      <h1>This is a test</h1>
+    </div>
+  );
+}
+
+export default SampleLinkedIn;
