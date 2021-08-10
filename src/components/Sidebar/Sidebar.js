53ae2b65e3d1b68abb342918aefbd880fd946b58
BluVshal <blu.vishal@gmail.com>
2021-08-10 23:54:19 +0400

    Completed Implementation of Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,11 +3,19 @@ import React from 'react'
 
 import './Sidebar.css';
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <img src="" alt=""/>
-                <Avatar />
+                <img src="https://cdn.wallpapersafari.com/2/72/8np2J4.jpg" alt=""/>
+                <Avatar className="sidebar__avatar"/>
                 <h2>User</h2>
                 <h4>user_sample@gmail.com</h4>
             </div>
@@ -16,13 +24,18 @@ function Sidebar() {
                     <p>Who viewed you?</p>
                     <p className="sidebar__statNumber">2,453</p>
                 </div>
-                <div className="sidebar_stat">
+                <div className="sidebar__stat">
                     <p>Views on post</p>
-                    <p className="sidebar__statNumber">3,456</p>
+                    <p className="sidebar__statNumber">2,345</p>
                 </div>
             </div>
             <div className="sidebar__bottom">
                 <p>Recent</p>
+                {recentItem("Python")}
+                {recentItem("React Js")}
+                {recentItem("Software Engineering")}
+                {recentItem("Front End Developer")}
+                {recentItem("NodeJs")}
             </div>
         </div>
     )
